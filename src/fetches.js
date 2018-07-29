9b0b00899e471671aa60ebf888a87c5ce6b78dd8
esharri2 <esharri2@gmail.com>
2018-07-29 13:01:10 -0400

    form set up

--- src/fetches.js
+++ src/fetches.js
@@ -1,11 +1,28 @@
+const endpoint = "https://api.worldbank.org/v2"
+
 export const topics = () =>
-    fetch(`https://api.worldbank.org/v2/topics?format=json`)
+    fetch(`${endpoint}/topics?format=json`)
+        // .then(response => console.log(response))
         .then(response => response.json())
         .then(response => response)
         .catch(error => console.error(`Fetch Error =\n`, error));
 
 export const indicators = (id) =>
-    fetch(`https://api.worldbank.org/v2/topics/${id}/indicators?format=json`)
+    fetch(`${endpoint}/topics/${id}/indicators?format=json&per_page=100`)
         .then(response => response.json())
         .then(response => response)
         .catch(error => console.error(`Fetch Error =\n`, error));
+
+export const countries = () =>
+    fetch(`${endpoint}/countries?format=json&per_page=500`)
+        .then(response => response.json())
+        .then(response => response)
+        .catch(error => console.error(`Fetch Error =\n`, error));
+
+export const chartData = (country, indicator) =>
+    fetch(`${endpoint}/countries/${country}/indicators/${indicator}?format=json&per_page=500&date=2000:2018`)
+        .then(response => response.json())
+        .then(response => response)
+        .catch(error => console.error(`Fetch Error =\n`, error));
+
+
--- src/index.js
+++ src/index.js
@@ -1,26 +1,43 @@
 import './stylesheets/styles.scss';
 import 'whatwg-fetch';
-import { topics, indicators } from './fetches'
-
+import { topics, indicators, countries, chartData } from './fetches';
+import createChart from './createChart';
 
 async function addTopics() {
     const data = await topics();
-    const markup = data[1].map(topic => `<option value="${topic.id}">${topic.value}</option>`);
-    document.querySelector("#topic").innerHTML = markup;    
-    document.querySelector("#topic option")
+    const markup = data[1].map(topic => `<option value=${topic.id}>${topic.value}</option>`);
+    const topicMenu = document.querySelector("#topic")
+    topicMenu.innerHTML = markup;
+    topicMenu.addEventListener("change", function () {
+        addIndicators(this.value);
+    })
 }
 
-async function addIndicators() {
-    const data = await indicators();
-
+async function addCountries() {
+    const data = await countries();
+    const markup = data[1].map(country => `<option value=${country.id}>${country.name}</option>`);
+    const countriesMenu = document.querySelector("#country")
+    countriesMenu.innerHTML = markup;
 }
 
+async function addIndicators(topicId) {
+    const data = await indicators(topicId);
+    const markup = data[1].map(indicator => `<option value=${indicator.id}>${indicator.name}</option>`);
+    document.querySelector("#indicator").innerHTML = markup;
+}
 
+document.querySelector("#submit").addEventListener("click", function (event) {
+    event.preventDefault();
+    const country = document.querySelector("#country").value;
+    const indicator = document.querySelector("#indicator").value;
+    getChartData(country, indicator);
 
+})
 
-
-
-
-
+async function getChartData(country, indicator) {
+    const data = await chartData(country, indicator);
+    createChart(data);
+}
 
 addTopics();
+addCountries();
